b2dc1781df1529ce3944e53b6c3fee7aa7599306
MATHEUS HENRY DOS SANTOS <25204051@CFP127ALUNO.LOCAL>
2025-09-22 16:58:43 -0300

    salvando as primeiras partes do projeto

--- src/br/senai/sp/jandira/ds1t/calculadoraDeImcParaAcademia/CalculadoraDeImcApp.java
+++ src/br/senai/sp/jandira/ds1t/calculadoraDeImcParaAcademia/CalculadoraDeImcApp.java
@@ -1,4 +1,10 @@
 package br.senai.sp.jandira.ds1t.calculadoraDeImcParaAcademia;
 
+import java.sql.SQLOutput;
+
 public class CalculadoraDeImcApp {
+
+    public static void main(String[] args){
+        System.out.println("Executando programa...");
+    }
 }
--- src/br/senai/sp/jandira/ds1t/calculadoraDeImcParaAcademia/model/CalculadoraDeImc.java
+++ src/br/senai/sp/jandira/ds1t/calculadoraDeImcParaAcademia/model/CalculadoraDeImc.java
@@ -1,4 +1,37 @@
 package br.senai.sp.jandira.ds1t.calculadoraDeImcParaAcademia.model;
 
+import java.util.Scanner;
+
 public class CalculadoraDeImc {
+
+    String nomeDoAluno;
+    double peso;
+    double altura;
+    double resultadoDoCalculo;
+
+    public void obterDados (){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Olá! Qual o nome do aluno(a) ?");
+        nomeDoAluno = scanner.nextLine();
+
+        System.out.print("Qual o peso  ?");
+        peso = scanner.nextLine();
+
+        System.out.print("Olá! Qual o seu nome ?");
+        altura = scanner.nextLine();
+
+    }
+
+    public void calcularImc (){
+
+    }
+
+    public void distinguirEstadoDoAluno(){
+
+    }
+
+    public void exibirResultados (){
+
+    }
 }
