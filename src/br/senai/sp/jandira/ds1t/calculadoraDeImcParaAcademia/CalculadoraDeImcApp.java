bba0f2df03ada3a6c064a460997f2a82e04652cd
mthenryx <09matheushenry@gmail.com>
2025-09-26 14:02:19 -0300

    Criação do sistema que calcula seu imc e diz como está seu estado físico

--- src/br/senai/sp/jandira/ds1t/calculadoraDeImcParaAcademia/CalculadoraDeImcApp.java
+++ src/br/senai/sp/jandira/ds1t/calculadoraDeImcParaAcademia/CalculadoraDeImcApp.java
@@ -1,10 +1,13 @@
 package br.senai.sp.jandira.ds1t.calculadoraDeImcParaAcademia;
 
-import java.sql.SQLOutput;
+import br.senai.sp.jandira.ds1t.calculadoraDeImcParaAcademia.model.CalculadoraDeImc;
 
 public class CalculadoraDeImcApp {
 
     public static void main(String[] args){
         System.out.println("Executando programa...");
+
+        CalculadoraDeImc imc  = new CalculadoraDeImc();
+        imc.obterDados();
     }
 }
--- src/br/senai/sp/jandira/ds1t/calculadoraDeImcParaAcademia/model/CalculadoraDeImc.java
+++ src/br/senai/sp/jandira/ds1t/calculadoraDeImcParaAcademia/model/CalculadoraDeImc.java
@@ -7,7 +7,8 @@ public class CalculadoraDeImc {
     String nomeDoAluno;
     double peso;
     double altura;
-    double resultadoDoCalculo;
+    double imc;
+    String estadoFisico;
 
     public void obterDados (){
         Scanner scanner = new Scanner(System.in);
@@ -15,23 +16,43 @@ public class CalculadoraDeImc {
         System.out.print("Olá! Qual o nome do aluno(a) ?");
         nomeDoAluno = scanner.nextLine();
 
-        System.out.print("Qual o peso  ?");
-        peso = scanner.nextLine();
+        System.out.print("Qual o peso de " + nomeDoAluno + "?");
+        peso = scanner.nextDouble();
 
-        System.out.print("Olá! Qual o seu nome ?");
-        altura = scanner.nextLine();
+        System.out.print("Qual a altura de " + nomeDoAluno + " em metros?");
+        altura = scanner.nextDouble();
 
+        calcularImc();
     }
 
     public void calcularImc (){
+        imc = peso/(altura * altura);
 
+        distinguirEstadoDoAluno();
     }
 
     public void distinguirEstadoDoAluno(){
+        if (imc >= 40){
+            estadoFisico = "OBESIDADE GRAU III(mórbida). É recomendado que este aluno procure um médico antes de iniciar ou intensificar os treinos.";
+        } else if (imc >= 35) {
+            estadoFisico = "OBESIDADE GRAU II (severa). É recomendado que este aluno procure um médico antes de iniciar ou intensificar os treinos.";
+        } else if (imc >= 30) {
+            estadoFisico = "OBESIDADE GRAU I. É recomendado que este aluno procure um médico antes de iniciar ou intensificar os treinos.";
+        } else if (imc >= 25) {
+            estadoFisico = "LEVEMENTE ACIMA DO PESO";
+        } else if (imc >= 18.6) {
+            estadoFisico = "PESO IDEAL (PARABÉNS)";
+        } else {
+            estadoFisico = "ABAIXO DO PESO. É recomendado que este aluno procure um médico antes de iniciar ou intensificar os treinos.";
+        }
 
+        exibirResultados();
     }
 
     public void exibirResultados (){
 
+        System.out.println("Aluno(a): " + nomeDoAluno);
+        System.out.println("O imc chegou no resultado de: " + imc);
+        System.out.println(nomeDoAluno + " se encontra em: " + estadoFisico);
     }
 }
